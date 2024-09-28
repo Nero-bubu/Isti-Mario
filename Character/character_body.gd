b78a3a56a2ddb50889483eaa4abe7e90e8c478b4
Toldy Tamás <toldy.tamas@petrik.hu>
2024-09-28 16:16:21 +0200

    V0.3.2

--- Character/character_body.gd
+++ Character/character_body.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var speed: float = 500.0 # Mozgási sebesség
 @export var jump_force: float = 1000.0 # Ugrás ereje
 var jump_count = 0
+var frame_timer = 0.1
+var animation_timer = 0.0
 
 func _physics_process(delta: float) -> void:
 	# Gravitáció alkalmazása
@@ -22,14 +24,28 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 
 	# Ugrás
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or jump_count < 2): 
+	if   is_on_floor():
+		jump_count = 0
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or jump_count == 1): 
 		velocity.y = -jump_force
 		jump_count = jump_count+1
-	if jump_count == 2 and is_on_floor():
-		jump_count = 0
+
 	
 	# A sebesség alkalmazása a testre
 	move_and_slide()
 	if position.y > 1000:  # Például ha a karakter 1000-nél mélyebbre esik
 		position.x = Global.checkpoint  # Visszaállítjuk a kezdő pozícióra
 		position.y = 10
+# Mozgás ellenőrzése
+	if Input.is_action_pressed("move_left"):
+		animation_timer += delta
+		if animation_timer >= frame_timer * 2:  # Lassítva a frame váltást
+			$Sprite2D.frame = 0 if $Sprite2D.frame == 1 else 1  # Váltás a 0 és 1 frame között
+			animation_timer = 0.0
+	elif Input.is_action_pressed("move_right"):
+		animation_timer += delta
+		if animation_timer >= frame_timer * 2:  # Lassítva a frame váltást
+			$Sprite2D.frame = 3 if $Sprite2D.frame == 4 else 4  # Váltás a 4 és 5 frame között
+			animation_timer = 0.0
+	else:
+		$Sprite2D.frame = 2  # Alapértelmezett frame ha nincs mozgás
